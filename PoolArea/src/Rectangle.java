c314ff47760843a71a16f362c4df361be82b8afe
GO58 <greg_borrow@hotmail.com>
2023-06-09 10:29:47 -0700

    Rectangle class test passes

--- PoolArea/src/Rectangle.java
+++ PoolArea/src/Rectangle.java
@@ -1,3 +1,21 @@
 public class Rectangle {
-    
+    private int length;
+    private int width;
+
+    public Rectangle(int width, int length) {
+        this.length = (length < 0) ? 0 : length;
+        this.width = (width < 0) ? 0 : width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getArea(){
+        return length * width;
+    }
 }
