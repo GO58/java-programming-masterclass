8cabcc996b800a238fb8e8424212ef1e75722d4e
G_O58 <greg_borrow@hotmail.com>
2023-06-19 10:47:19 -0700

    Added Ceiling class and tests

--- Section8/CompositionChallenge/src/App.java
+++ Section8/CompositionChallenge/src/App.java
@@ -24,6 +24,8 @@ public class App {
         Bed myBed = new Bed("Standard bed", 2, 28, 2, 1);
         System.out.println("The be is style " + myBed.getStyle());
         System.out.printf("is %d inches high, has %d pillows, %d sheets and %d quilts.%n", myBed.getHeight(), myBed.getPillows(), myBed.getSheets(), myBed.getQuilt());
+        Ceiling myCeiling = new Ceiling(10, 256);
+        System.out.printf("The ceiling is %d feet high and color number is %d.%n", myCeiling.getHeight(), myCeiling.getPaintedColor());
         System.out.println("----------end tests-----------");
     }
 }
