4afedd366691cefef4919398af7ff72dd5abf2af
Greg Borrow <greg_borrow@hotmail.com>
2023-06-22 11:33:18 -0700

    array randomized, sort and reverse

---
+++ Section9/Section9CodeAlong/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Start Tests");
+        arrayChallenge();
+        System.out.println("--------end tests----------");
+    }
+
+    private static void arrayChallenge(){
+        int[] randomArray = new int[22];
+        Random random = new Random();
+        for(int i = 0; i < randomArray.length; i++){
+            randomArray[i] = random.nextInt(1000);
+        }
+        System.out.println(Arrays.toString(randomArray));
+        int[] sortedArray = randomArray.clone();
+        Arrays.sort(sortedArray);
+        System.out.println(Arrays.toString(sortedArray));
+        int[] reversedArray = new int[sortedArray.length];
+        for(int i = 0; i < sortedArray.length; i++){
+            reversedArray[i] = sortedArray[sortedArray.length - 1 - i];
+        }
+        System.out.println(Arrays.toString(reversedArray));
+    }
+}
